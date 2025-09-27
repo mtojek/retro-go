46c3a309f1c837167203d0aeb470d31d9582bc9d
Marcin Tojek <marcin@coder.com>
2025-09-27 12:13:49 +0200

    initial config

--- components/retro-go/targets/mtojek-s3/config.h
+++ components/retro-go/targets/mtojek-s3/config.h
@@ -16,7 +16,7 @@
 // Video
 #define RG_SCREEN_DRIVER            0   // 0 = ILI9341/ST7789
 #define RG_SCREEN_HOST              SPI2_HOST
-#define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M // SPI_MASTER_FREQ_80M
+#define RG_SCREEN_SPEED             60000000 // SPI_MASTER_FREQ_40M
 #define RG_SCREEN_BACKLIGHT         1
 #define RG_SCREEN_WIDTH             320
 #define RG_SCREEN_HEIGHT            240
@@ -34,7 +34,7 @@
     ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
     ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
     ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(0x36, 0x68);                 /* Memory Access Control  (MX|MV|BGR) */                            \
+    ILI9341_CMD(0x36, 0xA8);                 /* Memory Access Control  (MY|MV|BGR) */                            \
     ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
     ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
     ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
@@ -43,22 +43,25 @@
     ILI9341_CMD(0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); \
     ILI9341_CMD(0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
 
-
 // Input
 // Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
-#define RG_GAMEPAD_ADC_MAP {\
+/*#define RG_GAMEPAD_ADC_MAP {\
     {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 3072, 4096},\
     {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1024, 3071},\
     {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_5, ADC_ATTEN_DB_11, 1024, 3071},\
     {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 3072, 4096},\
-}
+}*/
 // TODO RG_GAMEPAD_ LEFT/RIGHT/DOWN/UP
 #define RG_GAMEPAD_GPIO_MAP {\
+    {RG_KEY_UP,     .num = GPIO_NUM_15, .pullup = 1, .level = 0},\
+    {RG_KEY_RIGHT,  .num = GPIO_NUM_2,  .pullup = 1, .level = 0},\
+    {RG_KEY_DOWN,   .num = GPIO_NUM_3,  .pullup = 1, .level = 0},\
+    {RG_KEY_LEFT,   .num = GPIO_NUM_1,  .pullup = 1, .level = 0},\
     {RG_KEY_SELECT, .num = GPIO_NUM_16, .pullup = 1, .level = 0},\
     {RG_KEY_START,  .num = GPIO_NUM_17, .pullup = 1, .level = 0},\
     {RG_KEY_MENU,   .num = GPIO_NUM_18, .pullup = 1, .level = 0},\
     {RG_KEY_OPTION, .num = GPIO_NUM_8,  .pullup = 1, .level = 0},\
-    {RG_KEY_A,      .num = GPIO_NUM_15, .pullup = 1, .level = 0},\
+    {RG_KEY_A,      .num = GPIO_NUM_0,  .pullup = 1, .level = 0},\
     {RG_KEY_B,      .num = GPIO_NUM_5,  .pullup = 1, .level = 0},\
 }
 
@@ -66,19 +69,19 @@
 #define RG_BATTERY_DRIVER           1
 #define RG_BATTERY_ADC_UNIT         ADC_UNIT_1
 #define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_3
-#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3500.f) / (4200.f - 3500.f) * 100.f)
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 2700.f) / (3300.f - 2700.f) * 100.f)
 #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
 // Status LED
 #define RG_GPIO_LED                 GPIO_NUM_38
 
 // SPI Display (back up working)
-#define RG_GPIO_LCD_MISO            GPIO_NUM_NC
+#define RG_GPIO_LCD_MISO            GPIO_NUM_13
 #define RG_GPIO_LCD_MOSI            GPIO_NUM_11
 #define RG_GPIO_LCD_CLK             GPIO_NUM_12
 #define RG_GPIO_LCD_CS              GPIO_NUM_10
 #define RG_GPIO_LCD_DC              GPIO_NUM_46
-#define RG_GPIO_LCD_BCKL            GPIO_NUM_13
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_14
 #define RG_GPIO_LCD_RST             GPIO_NUM_9
 
 #define RG_GPIO_SDSPI_MISO          GPIO_NUM_40
@@ -91,3 +94,6 @@
 #define RG_GPIO_SND_I2S_WS          45
 #define RG_GPIO_SND_I2S_DATA        47
 #define RG_GPIO_SND_AMP_ENABLE      21
+
+// Other
+#define RG_UPDATER_ENABLE 0
