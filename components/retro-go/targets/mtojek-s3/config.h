bc25dd8820c9e4ed11416008e3e1831859ddd607
Marcin Tojek <marcin@coder.com>
2025-09-27 22:07:28 +0200

    Fix: battery calculation

--- components/retro-go/targets/mtojek-s3/config.h
+++ components/retro-go/targets/mtojek-s3/config.h
@@ -69,7 +69,7 @@
 #define RG_BATTERY_DRIVER           1
 #define RG_BATTERY_ADC_UNIT         ADC_UNIT_1
 #define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_3
-#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 2700.f) / (3300.f - 2700.f) * 100.f)
+#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3300.f) / (4200.f - 3300.f) * 100.f)
 #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
 // Status LED
